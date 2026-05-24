9fd6815b9add4bf252c4ad7c8bac59fb3c8a6876
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-24 22:43:55 +0800

    shell script changes

--- 01-shell-script.sh
+++ 01-shell-script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 echo "All variables passed to script: $@"
+echo "Number of vairables passed: $#"
+echo "First vairable: $1"
+echo "Script Name: $0"
+echo "Who is running this : $USER"
