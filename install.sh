f85c215306ae4c33861e424ace209cd572ef61f2
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-25 00:02:15 +0800

    shell script changes

--- install.sh
+++ install.sh
@@ -2,17 +2,11 @@
 
 USERID=$(id -u)
 
-echo "user id : $USERID"
-
 # Check root access or not
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access"
     exit 1
 fi
-
-# echo "I'm continuing"
-
-echo "Installing mysql"
 
 dnf list installed mysql
 if [ $? -eq 0 ]; then 
