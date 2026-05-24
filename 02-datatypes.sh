6cacb0ac8e2b08ddb6fff8f5e6017cdf95c8715a
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-24 23:12:19 +0800

    shell script changes

--- 02-datatypes.sh
+++ 02-datatypes.sh
@@ -10,3 +10,6 @@ echo "total: $sum"
 MOVIES = ("RRR" "Bahubali" "Pushpa")
 
 echo "Movies are : ${MOVIES[@]}"
+echo "First movie: ${MOVIES[0]}"
+echo "Second movie: ${MOVIES[1]}"
+echo "Third movie: ${MOVIES[2]}"
