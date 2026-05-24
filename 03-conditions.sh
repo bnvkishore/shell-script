1c4f141c955df9c313df8678f4f061cd381181fc
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-24 23:28:52 +0800

    shell script changes

---
+++ 03-conditions.sh
@@ -0,0 +1,13 @@
+
+number=$1
+
+# gt -> Grater than
+# lt -> Less than
+# eq -> Equals to
+# ne -> Not equal
+# ge -> Grater than or equal
+#le -> Less than or equal
+
+if [ $number -ge 20 ]; then
+    echo "Give number $number is grater than or equal to 20
+fi
