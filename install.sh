34045fb4bd280d8f8489c7758149a59de0d4616b
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-25 00:17:19 +0800

    shell script changes

--- install.sh
+++ install.sh
@@ -21,12 +21,12 @@ VALIDATE() {
     fi
 }
 
-dnf list installed mysql
-if [ $? -eq 0 ]; then 
-    echo "Mysql is already installed ... SKIPPING"
-else
-    VALIDATE mysql $?
-fi
+# dnf list installed mysql
+# if [ $? -eq 0 ]; then 
+#     echo "Mysql is already installed ... SKIPPING"
+# else
+#     VALIDATE mysql $?
+# fi
 
 dnf list installed nginx
 if [ $? -eq 0 ]; then 
