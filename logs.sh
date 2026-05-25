71310447f30b426a59fc097db38eb36672575f40
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-25 23:56:55 +0800

    shell script changes

--- logs.sh
+++ logs.sh
@@ -4,7 +4,7 @@
 
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
-LOG_FILE="$LOGS_DIR/$0.log"
+LOGS_FILE="$LOGS_DIR/$0.log"
 # Check root access or not
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access"
