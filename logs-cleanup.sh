680cc21c881523782525216dd5208e4b98dc5c37
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-31 23:31:05 +0800

    mongodb shell script added

--- logs-cleanup.sh
+++ logs-cleanup.sh
@@ -12,3 +12,16 @@ if [ ! -d $SOURCE_DIR ]; then
     echo "ERROR:: Directory $SOURCE_DIR does not exist"
     exit 1
 fi
+
+echo "Scanning source directory for logs files older than $DAYS days"
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+
+if [ -z $FILES ]; then
+    echo "No log files older than 14 days found"
+    exit 0
+else
+    while IFS= read -r FILE
+    do 
+        echo "file to be deleted: $FILE"
+    done <<<  "$FILES"
+fi
