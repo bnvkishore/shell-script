648be063d31f1f8c6e32b81a4d2cbeccc0efb01c
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-31 23:59:20 +0800

    mongodb shell script added

--- logs-cleanup.sh
+++ logs-cleanup.sh
@@ -19,11 +19,11 @@ FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 if [ -z "$FILES" ]; then
     echo "No log files older than 14 days found"
     exit 0
-else
-    while IFS= read -r FILE
-    do 
-        echo "file to be deleted: $FILE"
-        rm -f $FILE
-        echo "File $FILE deleted"
-    done <<<  "$FILES"
 fi
+
+while IFS= read -r FILE
+do 
+    echo "file to be deleted: $FILE"
+    rm -f $FILE
+    echo "File $FILE deleted"
+done <<<  "$FILES"
