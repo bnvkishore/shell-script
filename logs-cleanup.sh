3df2fcf2f8afded88186f72675d5f18816b0027d
bnvkishore <kishoreb.n.v209@gmail.com>
2026-05-31 23:21:26 +0800

    mongodb shell script added

--- logs-cleanup.sh
+++ logs-cleanup.sh
@@ -2,7 +2,7 @@
 SOURCE_DIR=$1
 DAYS=${2:-14}
 
-if [ -z SOURCE_DIR ]; then
+if [ -z $SOURCE_DIR ]; then
     echo "ERROR:: Missing parameters"
     echo "USAGE: $0 <source-dir> [days(optional default to 14)]"
 fi
