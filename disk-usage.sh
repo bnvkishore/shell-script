8b6cb7d2f8d19c499876fa6396cb17ab13b4ee9d
bnvkishore <kishoreb.n.v209@gmail.com>
2026-06-01 23:19:54 +0800

    mongodb shell script added

--- disk-usage.sh
+++ disk-usage.sh
@@ -9,7 +9,7 @@ do
     USAGE=$(echo $LINE | awk '{print $6}' | cut -d "%" -f1 )
     PARTITION=$(echo $LINE | awk '{print $7}' )
     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-        MESSAGE="High disk usage on $PARTITION: $USAGES"
+        MESSAGE+="High disk usage on $PARTITION: $USAGES"
     fi
     echo $LINE
 done <<< $DISK_USAGE
